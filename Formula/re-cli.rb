c3c88c3cd2ad695635abff4133977680f32bf845
Tanner Bennett <tannerbennett@me.com>
2026-07-01 06:13:09 -0500

    re-cli 0.3.6

--- Formula/re-cli.rb
+++ Formula/re-cli.rb
@@ -1,8 +1,8 @@
 class ReCli < Formula
   desc "Reverse engineering CLI wrapping IDA Pro and Hopper Disassembler"
   homepage "https://github.com/NSExceptional/re-cli"
-  url "https://github.com/NSExceptional/re-cli/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "36ab2824509b676577a03098e359bc37cb7d1e63a507fb232c8a34d45d3f0af9"
+  url "https://github.com/NSExceptional/re-cli/archive/refs/tags/v0.3.6.tar.gz"
+  sha256 "ab762c0fa701def8e4839b6d82ef74f271fa193384c4fd7f7ff00d8fddec052e"
   license "MIT"
   head "https://github.com/NSExceptional/re-cli.git", branch: "main"
 
